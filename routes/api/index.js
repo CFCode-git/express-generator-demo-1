247c11fd07eec5517bf5d93eb9147c64898c7dc4
周啟尧 <435184766@qq.com>
2022-01-13 00:15:38 +0800

    update:通过jwt进行鉴权

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,18 +3,25 @@ const express = require('express');
 const router = express.Router();
 
 const usersRouter = require('./users')
+const JWT = require('jsonwebtoken')
 
 const users = []
 
 router.get('/login',(req,res,next)=>{
   const {username} = req.query
-  req.session.user = {username}
-  res.send()
+  const user = {username, expireAt:Date.now().valueOf() + (20*60*1000)}
+  const token = JWT.sign(user,'adsfaosdfasdfasdf')
+  res.send(token)
 })
 
 router.get('/hello',(req,res,next)=>{
-  const {username} = req.session
-  res.send(`<h1>hello,${req.cookies.username}</h1>`)
+  const auth = req.get('Authorization')
+  if(!auth) return res.send('no auth')
+  if(auth.indexOf('Bearer ') ===  -1) return res.send('no auth')
+  const token = auth.split('Bearer ')[1]
+  const user = JWT.verify(token,'adsfaosdfasdfasdf')
+  if(user.expireAt < Date.now().valueOf())res.send('token 失效')
+  res.send(user)
 })
 
 router.use('/users',usersRouter)
